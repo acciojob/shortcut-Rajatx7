8ba6d22acd9cd6619b3db455c051c94fa003c94c
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-20 14:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1=="" || s2==""){
+		return '';
+	}
+else{
+	let str=s1.chatAt(0)+s1.chatAt(0);
+	return str;
+}
+
+
+
+
+
+
+	
 }
 
 // Do not change the code below.
